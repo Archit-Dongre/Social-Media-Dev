19cf509585f3c99c8b336f429a4e04ffbfe8860f
Archit <dongrearchit@gmail.com>
2021-04-09 15:46:39 +0530

    added post creation security at controller level

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const postsController = require("../controllers/postsController");
+const passport = require("passport")
 
-
-
-router.post("/createPost" , postsController.createPost);
+//putting an authentication check before creation of post so somebody dosent manually create
+//another form and send to my post link 
+router.post("/createPost" ,passport.checkAuthentication, postsController.createPost);
 
 module.exports = router;
